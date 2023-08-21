945e7f68e3105ded9a49bd1ce0d74575732f1da4
Allen Yu <allen.yu@wayne.edu>
2023-08-21 17:52:57 -0400

    Added Category tab
    Added table to category tab
    Added functionality
    -Checking values
    -add/edit/delete (delete WIP)
    added some UI styling

--- Bulky/BulkyWeb/Controllers/CategoryController.cs
+++ Bulky/BulkyWeb/Controllers/CategoryController.cs
@@ -1,12 +1,66 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers
 {
-    public class CategoryController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
+	public class CategoryController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+		public CategoryController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+		public IActionResult Index()
+		{
+			List<Category> objCategoryList = _db.Categories.ToList();
+			return View(objCategoryList);
+		}
+
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Create(Category obj)
+		{
+			//if (obj.Name != null && obj.Name == obj.DisplayOrder.ToString())
+			//{
+			//	ModelState.AddModelError("Name", "The DisplayOrder cannot exactly match the name.");
+			//	ModelState.AddModelError("DisplayOrder", "The DisplayOrder cannot exactly match the name.");
+			//}
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Add(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index", "Category");
+			}
+			return View();
+		}
+		[HttpGet]
+		public IActionResult Edit(int? id)
+		{
+            if (id == null || id == 0)
+            {
+				return NotFound();   
+            }
+			Category categoryFromDb = _db.Categories.Find(id);
+			if (categoryFromDb == null)
+				return NotFound();
+            return View(categoryFromDb);
+		}
+		[HttpPost]
+		public IActionResult Edit(Category obj)
+		{
+			
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Add(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index", "Category");
+			}
+			return View();
+		}
+	}
 }
--- Bulky/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ Bulky/BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2b19640f3620597870d6c8a6ef265fd11892723")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3316500b19fa85e65d2f8a1bb89051bfac0d1744")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
